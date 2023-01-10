6164d7bba403c0ed2131527a302c06a4a85d0e3a
DFinelli <danielfinelli@Daniels-MacBook-Pro.local>
2023-01-10 10:38:48 +0100

    model, repo, data bones

--- studentsystem/src/main/java/com/DFinelli/studentsystem/repository/StudentRepository.java
+++ studentsystem/src/main/java/com/DFinelli/studentsystem/repository/StudentRepository.java
@@ -1,5 +1,10 @@
 package com.DFinelli.studentsystem.repository;
 
-public interface StudentRepository {
+import com.DFinelli.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
 }
